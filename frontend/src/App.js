d87145f6a0cffce664b0778af011d3463e735338
Tim Abrahamson <tim@kinetic.com>
2020-10-06 12:05:31 -0500

    Updated url on the App page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -21,6 +21,7 @@ import CreateNewJobListing from "./pages/CreateNewJobListing";
 import UpdateJobListing from "./pages/UpdateJobListing";
 
 function App() {
+  const url = "https://jr-dev-sim-backend.herokuapp.com";
   const [userData, setUserData] = useState({});
 
   useEffect(() => {
@@ -29,7 +30,7 @@ function App() {
       if (token === null) {
         localStorage.setItem("auth-token", "");
       }
-      const getUser = await axios.get("http://localhost:5000/api/user", {
+      const getUser = await axios.get(`${url}/api/user`, {
         headers: { "auth-token": token }
       });
       setUserData({
